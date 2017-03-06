0d84c9cc7950f26ac50bc5597e9d38316f3e582a
git-clay <ce.smith5525@gmail.com>
2017-03-06 09:54:30 -0700

    creating h1-h5 for each color class

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var concat = require('gulp-concat'); //concat files from common directory
+var sass = require('gulp-sass'); //sass->css
+var minifyCss = require('gulp-minify-css'); //minify css
+var rename = require('gulp-rename'); // allows to rename file (.min.css)
+var plumber = require('gulp-plumber');
+var liveReload = require('gulp-livereload');
+
+
+
+var paths = {
+  sass: ['./scss/**/*.scss',
+         './scss/*.scss'],
+};
+
+gulp.task('sass', function(done) {
+  gulp.src('./scss/splotch.scss')
+    .pipe(plumber(function(error){
+      gutil.log(error.message);
+      this.emit('end');
+    }))
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./dist/css/'))
+    .on('end', done);
+});
+
+
+gulp.task('watch', function() {
+  liveReload.listen();
+  gulp.watch(paths.sass, ['sass']);
+});
+
+
+
+gulp.task('default', ['sass','watch']);
+
